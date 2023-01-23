5492bbd7dab17959ea195f6568853b68216183d2
Eddie Antonio Santos <eddie.santos@ucdconnect.ie>
2023-01-23 22:55:50 +0000

    refactor: change name of crate to hx

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,6 @@ use std::fs;
 fn main() {
     let filename = std::env::args().nth(1).unwrap();
     let file = fs::read(filename).unwrap();
-    let buffer = print_hex::simd_2(&file);
+    let buffer = hx::simd_2(&file);
     println!("{buffer}");
 }
